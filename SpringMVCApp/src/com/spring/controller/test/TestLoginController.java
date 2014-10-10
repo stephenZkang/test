0c2444467f3d42b10892e96de97766763e609c6e
stphenZkang <qiao54007@gmail.com>
2014-10-10 09:55:26 +0800

    将servlet-api.jar修改为3.0版本的

--- SpringMVCApp/src/com/spring/controller/test/TestLoginController.java
+++ SpringMVCApp/src/com/spring/controller/test/TestLoginController.java
@@ -1,8 +1,9 @@
 package com.spring.controller.test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import org.junit.Before;
@@ -11,8 +12,9 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.spring.controller.LoginController;
 
@@ -26,16 +28,33 @@ public class TestLoginController {
 	@Before
 	public void setUp(){
 		MockitoAnnotations.initMocks(this);
-		this.mockMvc = standaloneSetup(controller).build();
+		/**
+		 * 使用standaloneSetup来安装controller
+		 */
+		this.mockMvc = standaloneSetup(controller)
+							.setViewResolvers(viewResolver())
+							.build();
 	}
 	
+	private ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+	}
+	
+	/**
+	 * @throws Exception
+	 * 
+	 * @see 测试LoginController中的helloworld方法
+	 */
 	@Test
 	public void thatHelloWorldAfterThisHttpOK() throws Exception{
 		
-		this.mockMvc.perform(post("helloworld")
-				.contentType(MediaType.APPLICATION_XHTML_XML))
-				.andDo(print())
-				.andExpect(status().isOk());
+		this.mockMvc.perform(post("/helloWorld"))
+				.andExpect(status().isOk())			//返回ok的话
+                .andExpect(view().name("helloworld"))		//映射到helloworld
+                .andExpect(forwardedUrl("/WEB-INF/views/helloworld.jsp"));	//跳转的页面
 	}
 
 	
