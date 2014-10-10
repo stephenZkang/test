d730864053c840d7f78c45d97cb9cd3f15cd031d
stphenZkang <qiao54007@gmail.com>
2014-10-10 10:57:00 +0800

    Spring Junit LogonController的单元测试;基于MockMvcBuilders.standaloneSetup的 实现

--- SpringMVCApp/src/com/spring/controller/LoginController.java
+++ SpringMVCApp/src/com/spring/controller/LoginController.java
@@ -2,14 +2,19 @@ package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/helloWorld")
-	public String helloWorld(Model model){
-		model.addAttribute("message", "Hello World");
-		return "helloworld";
+	@RequestMapping("/helloWorld/{id}")
+	public String helloWorld(@PathVariable("id")Long id,Model model){
+		if(id == 1l){
+			throw new RuntimeException();
+		}else{
+			model.addAttribute("message", "Hello World");
+		}
+		return "views/helloworld";
 	}
 }
--- SpringMVCApp/src/com/spring/controller/test/TestLoginController.java
+++ SpringMVCApp/src/com/spring/controller/test/TestLoginController.java
@@ -5,6 +5,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+import java.util.Properties;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,11 +16,33 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.spring.controller.LoginController;
 
+/**
+ * @author qiaok
+ * 
+ * @see 
+ * 		MockMvcBuilders.standaloneSetup		//设置controller对象,
+ * 			.setHandlerExceptionResolvers(exceptionResolvers())	//设置异常映射
+			.setViewResolvers(viewResolver())	//设置跳转映射
+			
+		Test方法中使用
+			mockMvc.perform(post('helloworld/{id}',1l)) 
+				post请求协议,	helloworld/{id}映射路径,1l对应id传输的值
+			.andExpect(status().isOk())		
+				//期待返回的状态码: 有客户端错误，服务端错误，未发现，ok
+            .andExpect(view().name("views/helloworld"))		
+            	//映射到helloworld
+            .andExpect(model().attribute("message", "Hello World"))
+            	//模型中的参数值比对
+            .andExpect(forwardedUrl("/WEB-INF/views/helloworld.jsp"));	
+            	//期待跳转的页面
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class TestLoginController {
 	private MockMvc mockMvc;
@@ -32,13 +57,34 @@ public class TestLoginController {
 		 * 使用standaloneSetup来安装controller
 		 */
 		this.mockMvc = standaloneSetup(controller)
+							.setHandlerExceptionResolvers(exceptionResolvers())
 							.setViewResolvers(viewResolver())
 							.build();
 	}
 	
+
+
+
+	private HandlerExceptionResolver  exceptionResolvers() {
+		SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
+		Properties exceptionMappings = new Properties();
+		exceptionMappings.put("java.lang.Exception", "error/error");
+	    exceptionMappings.put("java.lang.RuntimeException", "error/error");
+	 
+		exceptionResolver.setExceptionMappings(exceptionMappings);
+		
+		Properties statusCodes = new Properties();
+		statusCodes.put("error/error", "500");
+		exceptionResolver.setStatusCodes(statusCodes);
+		return exceptionResolver;
+	}
+
+
+
+
 	private ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setPrefix("/WEB-INF/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
 	}
@@ -51,11 +97,24 @@ public class TestLoginController {
 	@Test
 	public void thatHelloWorldAfterThisHttpOK() throws Exception{
 		
-		this.mockMvc.perform(post("/helloWorld"))
+		this.mockMvc.perform(post("/helloWorld/{id}",2l))
 				.andExpect(status().isOk())			//返回ok的话
-                .andExpect(view().name("helloworld"))		//映射到helloworld
+                .andExpect(view().name("views/helloworld"))		//映射到helloworld
+                .andExpect(model().attribute("message", "Hello World"))
                 .andExpect(forwardedUrl("/WEB-INF/views/helloworld.jsp"));	//跳转的页面
 	}
 
-	
+
+	/**
+	 * @throws Exception
+	 * 
+	 * @see 测试LoginController中的helloworld方法
+	 */
+	@Test
+	public void thatHelloWorldAfterThisHttpERROR() throws Exception{
+		
+		this.mockMvc.perform(post("/helloWorld/{id}",1l))
+				.andExpect(status().is5xxServerError())			//返回ok的话
+                .andExpect(forwardedUrl("/WEB-INF/error/error.jsp"));	//跳转的页面
+	}
 }
