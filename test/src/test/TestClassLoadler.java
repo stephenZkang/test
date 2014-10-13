3f21918ae107c138dd83ca05c11b33a16f9a50c2
stphenZkang <qiao54007@gmail.com>
2014-10-13 15:09:16 +0800

    测试未通过,与预测的结果不同
    Signed-off-by: stphenZkang <qiao54007@gmail.com>

--- test/src/test/TestClassLoadler.java
+++ test/src/test/TestClassLoadler.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.junit.Test;
 
 import classloader.ClassLoaderA;
@@ -30,6 +33,31 @@ public class TestClassLoadler {
 		
 		System.out.println(object instanceof DomainA);
 		
+		
+		ClassLoader classLoader = new ClassLoader() {
+			@Override
+			public Class<?> loadClass(String name)
+					throws ClassNotFoundException {
+				try {
+					String fileName = name.substring(name.lastIndexOf(".")+1)+".class";
+					InputStream inputStream = getClass().getResourceAsStream(fileName);
+					if(inputStream == null){
+						return super.loadClass(name);
+					}
+					byte[] b;
+					b = new byte[inputStream.available()];
+					inputStream.read(b);
+					return defineClass(name, b, 0,b.length);
+				} catch (IOException e) {
+					throw new ClassNotFoundException();
+				}
+			}
+		};
+		
+		
+		Object object2 = classLoader.loadClass("classloader.DomainA").newInstance();
+		System.out.println(object2.getClass());
+		System.out.println(object2 instanceof classloader.DomainA);
 		
 	}
 }
