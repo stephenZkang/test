8c14de01b4025b486a20b492269a313d0ef734bd
stphenZkang <qiao54007@gmail.com>
2014-10-13 10:07:35 +0800

    配置Hibernate数据库连接信息
    Signed-off-by: stphenZkang <qiao54007@gmail.com>

---
+++ SpringMVCApp/src/com/spring/domain/base/BaseVo.java
@@ -0,0 +1,29 @@
+package com.spring.domain.base;
+
+/**
+ * @author qiaok
+ * @see 基础BaseVo
+ */
+public class BaseVo {
+	
+	private long id;
+	
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ SpringMVCApp/src/com/spring/service/test/TestPersonService.java
@@ -0,0 +1,5 @@
+package com.spring.service.test;
+
+public class TestPersonService {
+
+}
