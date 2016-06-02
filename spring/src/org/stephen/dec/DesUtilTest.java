b5eb7561a76aea06baaf7208f80264e584b51035
stphenZkang <qiao54007@gmail.com>
2016-06-02 09:42:31 +0800

    spring加密

---
+++ spring/src/org/stephen/dec/DesUtilTest.java
@@ -0,0 +1,11 @@
+package org.stephen.dec;
+
+import com.google.code.springcryptoutils.core.cipher.symmetric.Base64EncodedCiphererWithStaticKey;
+import com.google.code.springcryptoutils.core.cipher.symmetric.Base64EncodedCiphererWithStaticKeyImpl;
+
+public class DesUtilTest {
+	public static void main(String[] args) {
+		String content = "142430198801012314";
+		 
+	}
+}
