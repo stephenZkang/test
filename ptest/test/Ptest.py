de357830a3f9dcf9ed5334cdeb6b821f0a1335f6
stphenZkang <qiao54007@gmail.com>
2016-08-16 10:27:11 +0800

    读写文件

--- ptest/test/Ptest.py
+++ ptest/test/Ptest.py
@@ -4,29 +4,44 @@ Created on 2016-8-9
 
 @author: qiao123
 '''
-print "hello,world,中国";
+# print "hello,world,中国";
+# 
+# i = ['a','b']
+# l = [1,2]
+# 
+# print dict([i,l])
+# 
+# if 1==1:
+#     print 'hello,world'
+#     
+# i = [1,2,3,4]
 
-i = ['a','b']
-l = [1,2]
-
-print dict([i,l])
-
-if 1==1:
-    print 'hello,world'
-    
-i = [1,2,3,4]
-
-for v in i:
-    if v == 1:
-        print 'hello world'
-    elif v ==2:
-        print 'yes or no'
-    elif v ==3:
-        print 'very good'
-    else:
-        print 'pretty girl'
+# for v in i:
+#     if v == 1:
+#         print 'hello world'
+#     elif v ==2:
+#         print 'yes or no'
+#     elif v ==3:
+#         print 'very good'
+#     else:
+#         print 'pretty girl'
 
 
-#命令行输入
-s = raw_input("请输入:")
-print s
+#命令行输入 如：hello world
+#s = raw_input("请输入:")
+#print s
+
+#输入表达式    如：1+2 结果为3
+#s = input("欢迎：")
+#print s
+
+#打开关闭文件
+# fo = open("d://log.txt","r")
+# print "文件名",fo.name
+# 
+# #读文件
+# s = fo.read(20)
+# print s
+# 
+# fo.close()
+
