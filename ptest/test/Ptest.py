de640ec97538727092726fe46f9433e7304c6da9
stphenZkang <qiao54007@gmail.com>
2016-08-16 10:05:55 +0800

    python

---
+++ ptest/test/Ptest.py
@@ -0,0 +1,31 @@
+#coding=utf-8
+'''
+Created on 2016-8-9
+
+@author: qiao123
+'''
+print "hello,world,中国";
+
+i = ['a','b']
+l = [1,2]
+
+print dict([i,l])
+
+if 1==1:
+    print 'hello,world'
+    
+i = [1,2,3,4]
+
+for v in i:
+    if v == 1:
+        print 'hello world'
+    elif v ==2:
+        print 'yes or no'
+    elif v ==3:
+        print 'very good'
+    else:
+        print 'pretty girl'
+
+
+#命令行输入
+
