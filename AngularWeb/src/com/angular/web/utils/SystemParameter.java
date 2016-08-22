edb72e62958dd88a2f0e652acbc7653086b821e4
stphenZkang <qiao54007@gmail.com>
2016-08-22 14:57:03 +0800

    学习Angular 	工具类和系统常量

---
+++ AngularWeb/src/com/angular/web/utils/SystemParameter.java
@@ -0,0 +1,10 @@
+package com.angular.web.utils;
+
+/**
+ * 系统常量
+ * @author QIAOK
+ * @see 2016-8-23
+ */
+public class SystemParameter {
+
+}
---
+++ AngularWeb/src/com/angular/web/utils/ToolUtils.java
@@ -0,0 +1,13 @@
+package com.angular.web.utils;
+
+/**
+ * π§æﬂ¿‡
+ * @author QIAOK
+ * @see 2016-8-23
+ */
+public class ToolUtils {
+	public static void main(String[] args) {
+		
+	}
+
+}
