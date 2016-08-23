05803fb04731605fbaf73d92a01559477e93d44f
stphenZkang <qiao54007@gmail.com>
2016-08-23 12:39:02 +0800

    Angular Web目录组织未成功

--- AngularWeb1.0/WebContent/page/namesController.js
+++ AngularWeb1.0/WebContent/page/namesController.js
@@ -1,26 +1,27 @@
-angular.module('mainApp', []).controller('mainCtrl', function($scope,$http,$timeout) {
+angular.module('myApp')
+.controller('mainCtrl', function($scope,$http,$timeout) {
 	$scope.names=[
-       {name:'Jani',country:'Norway',color:'red'},
-       {name:'Hege',country:'Sweden',color:'yellow'},
-       {name:'Kai',country:'Denmark',color:'blue'}
-    ];
-	$timeout($timeout(function() {
-		$scope.names=[
-          {name:'科比',country:'American',color:'green'},
-          {name:'龙五',country:'HongKong',color:'blue'},
-          {name:'发哥',country:'ZiJiage',color:'orange'}
-       ];
-	}, 2000));
-	
-	$scope.firstName = "John";
-    $scope.lastName = "Doe";
-    $scope.myVar = false;
-    $scope.toggle = function() {
-        $scope.myVar = !$scope.myVar;
-    };
-    
-	/*$http.get('names.json').success(function(response) {
-		$scope.names =response.records;
-		
-	});*/
+	              {name:'Jani',country:'Norway',color:'red'},
+	              {name:'Hege',country:'Sweden',color:'yellow'},
+	              {name:'Kai',country:'Denmark',color:'blue'}
+	           ];
+	       	$timeout($timeout(function() {
+	       		$scope.names=[
+	                 {name:'科比',country:'American',color:'green'},
+	                 {name:'龙五',country:'HongKong',color:'blue'},
+	                 {name:'发哥',country:'ZiJiage',color:'orange'}
+	              ];
+	       	}, 2000));
+	       	
+	       	$scope.firstName = "John";
+	           $scope.lastName = "Doe";
+	           $scope.myVar = false;
+	           $scope.toggle = function() {
+	               $scope.myVar = !$scope.myVar;
+	           };
+	           
+	       	/*$http.get('names.json').success(function(response) {
+	       		$scope.names =response.records;
+	       		
+	       	});*/
 });
